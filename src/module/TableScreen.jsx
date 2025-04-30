e3cb774f92aaf55fdadc6a0e6c279c42b56a5701
Farky8 <pepefarky@gmail.com>
2025-04-30 09:17:35 +0200

    Added columns

--- src/module/TableScreen.jsx
+++ src/module/TableScreen.jsx
@@ -5,6 +5,48 @@ import { useTranslation } from 'react-i18next';
 export function TableScreen(props) {
 	const { t } = useTranslation();
 
+	const columns = [
+		{
+			title: "Username",
+			thStyle: {minWidth: "2rem"},
+			render: ({ row }) =>
+				<Link to={`/auth/session/${row._id}`}>
+					{row._id}
+				</Link>
+		},
+		{
+			title: "Email",
+			thStyle: {minWidth: "2rem"},
+			render: ({ row }) =>
+				<Link to={`/auth/credentials/${row.credentials_id}`}>
+					{row.credentials_id}
+				</Link>
+		},
+		{
+			title: "Created at",
+			thStyle: {minWidth: "4rem"},
+			render: ({ row }) => <DateTime value={row._c}/>
+		},
+		{
+			title: "Last signed in",
+			thStyle: {minWidth: "4rem"},
+			render: ({ row }) => <DateTime value={row.expiration}/>
+		},
+		{
+			title: "Address",
+			thStyle: {minWidth: "6rem"},
+			render: ({ row }) => <DateTime value={row.expiration}/>
+		},
+		{
+			thStyle: {width: "0px"},
+			tdStyle: {padding: "0px", whiteSpace: "nowrap"},
+			render: ({ row, column }) => (<>
+				{/* TODO, redirect to the detailed page */}
+				<button className="btn btn-primary me-1" onClick={() => onYClick(row)}><i className="bi bi-info-lg"></i></button>
+			</>)
+		}
+	];
+
 	return (
 		<Container className='h-100'>
 			{t('Training|Hello, there is nothing here yet!')}
